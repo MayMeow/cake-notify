9100a8c6950598167690ea94ba9f1127fda52f7d
MayMeow <emma@themaymeow.com>
2021-07-23 16:20:04 +0200

    Add users migrations

---
+++ config/Migrations/20210723141910_AddApiKeyToUsers.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddApiKeyToUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('api_key', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('api_key_plain', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
